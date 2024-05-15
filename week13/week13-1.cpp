2f60198a6486e192a0abe01625876cb7a4f9fcf3
CHiiii5640 <12750211@me.mcu.edu.tw>
2024-05-15 12:03:07 +0800

    week13

---
+++ week13/week13-1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void myprint(int x[3][4])
+{
+    for (int i=0;i<3;i++)
+    {
+        for (int j=0;j<4;j++)
+        {
+            printf("%2d ",x[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int a[3][4]; ///¨S­È¶Ã½X
+    int b[3][4] = {1,2,3};
+    int c[3][4] = { {1,2,3,4} , {5,6,7,8} , {9,10,11,12} };
+    myprint(a);
+    myprint(b);
+    myprint(c);
+}
